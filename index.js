2c8f820b4fb1db69c7318e1bfa670bfbbf1152eb
Mark <marksreese@gmail.com>
2022-05-02 15:42:34 -0700

    commit tree 0b28a53ab7ab505fe7cb9350ec9712176410c677

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array=[], event){
+    const returnArray=[];
+    for (let counter=0; counter<array.length; counter++){
+        returnArray.push(`Thank you, ${array[counter]}, for the wonderful ${event} gift!`);
+    }
+    return returnArray;
+}
+
+function countDown(integer){
+    while (integer>=0){
+        console.log(integer);
+        integer--;
+    }
+}
